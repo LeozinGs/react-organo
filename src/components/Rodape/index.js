aef3ffef742210cb2af5345bebdc6b5e4355a314
Leonardo <lokinho_ddtank@hotmail.com>
2024-09-05 02:15:52 -0300

    💄 Altera css do componente Rodape.

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -5,9 +5,9 @@ const Rodape = () => {
         <footer className='rodape'>
             <div className='container-rodape'>
                 <div className='container-botoes'>
-                    <a href="#"><img src="/images/fb.png" alt="Ícone do Facebook" /></a>
-                    <a href="#"><img src="/images/tw.png" alt="Ícone do Instagram" /></a>
-                    <a href="#"><img src="/images/ig.png" alt="Ícone do Twitter" /></a>
+                    <a href="http://www.facebook.com/"><img src="/images/fb.png" alt="Ícone do Facebook" /></a>
+                    <a href="http://www.twitter.com/"><img src="/images/tw.png" alt="Ícone do Instagram" /></a>
+                    <a href="http://www.instagram.com/"><img src="/images/ig.png" alt="Ícone do Twitter" /></a>
                 </div>
                 <img src="/images/logo.png" alt="Logo do Organo" />
                 <p>Desenvolvido por Alura.</p>

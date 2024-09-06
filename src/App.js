eb9aceb850a528608ffe81a467678712d6096478
Leonardo <lokinho_ddtank@hotmail.com>
2024-09-06 18:08:06 -0300

    ✨ Adiciona formulário para criar um novo time.

--- src/App.js
+++ src/App.js
@@ -231,12 +231,17 @@ function App() {
     }));
   }
 
+  function cadastrarTime(novoTime) {
+    setTimes([...times, { ...novoTime, id: uuidv4() }])
+  }
+
   return (
     <div>
 
       <Banner />
 
       <Formulario
+        cadastrarTime={cadastrarTime}
         times={times.map(time => time.nome)}
         aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])}
       />

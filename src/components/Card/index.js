d23fd87f0abe69f8d0c36ed58883f323c18624d6
Leonardo <lokinho_ddtank@hotmail.com>
2024-09-06 13:35:26 -0300

    💄 Estiliza o botão deletar usando o react-icons e css.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,10 @@
 import './Card.css';
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const Card = ({ nome, imagem, cargo, corDeFundo, aoDeletar }) => {
     return (
         <div className="colaborador">
-            <div className="deletar" onClick={aoDeletar}>deletar</div>
+            <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar} />
             <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
                 <img src={imagem} alt={nome} />
             </div>
